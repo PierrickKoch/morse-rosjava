7460b22a0b3fbdf1faa9e3da9568da3850caae55
Damien Cassou <damien.cassou@gmail.com>
2011-06-18 22:33:41 +0200

    Makes LaserScanMotion easier to understand

--- LaserScanMotion.java
+++ LaserScanMotion.java
@@ -6,74 +6,95 @@ import org.ros.MessageListener;
 import org.ros.Node;
 import org.ros.NodeConfiguration;
 import org.ros.NodeMain;
-import org.ros.Subscriber;
 import org.ros.Publisher;
-
-import org.ros.message.sensor_msgs.LaserScan;
 import org.ros.message.geometry_msgs.Twist;
+import org.ros.message.sensor_msgs.LaserScan;
 
 public class LaserScanMotion implements NodeMain {
 
-  private Node node;
+	private Node node;
 
-  @Override
-  public void main(NodeConfiguration configuration) {
-    Preconditions.checkState(node == null);
-    Preconditions.checkNotNull(configuration);
-    try {
-      node = new Node("laser_cmd", configuration);
-      final Publisher<Twist> publisher =
-          node.createPublisher("/ATRV/Motion_Controller", Twist.class);
+	@Override
+	public void main(NodeConfiguration configuration) {
+		Preconditions.checkState(node == null);
+		Preconditions.checkNotNull(configuration);
+		try {
+			node = new Node("laser_cmd", configuration);
+			Log log = node.getLog();
+			log.info("init");
+			node.createSubscriber( //
+					"/ATRV/Sick", //
+					new MyListener( //
+							log, //
+							node.createPublisher("/ATRV/Motion_Controller", Twist.class)), //
+					LaserScan.class);
+			log.info("ready");
+		} catch (Exception e) {
+			if (node != null) {
+				node.getLog().fatal(e);
+			} else {
+				e.printStackTrace();
+			}
+		}
+	}
 
-      final Log log = node.getLog();
-      log.info("init");
-      node.createSubscriber("/ATRV/Sick", new MessageListener<LaserScan>() {
-        @Override
-        public void onNewMessage(LaserScan msg) {
-          int i, mid = msg.ranges.length / 2;
-          Twist cmd = new Twist();
-          // halt if an object is less than 2m in a 30deg angle
-          boolean halt = false;
-          for (i = mid - 15; i <= mid + 15; i++) {
-            if (msg.ranges[i] < 2.0) {
-              halt = true;
-              break;
-            }
-          }
-          if (halt) {
-            double midA = 0, midB = 0;
-            // we go to the highest-range side scanned
-            for (i = 0; i < mid; i++)
-              midA += msg.ranges[i];
-            for (i = mid; i < msg.ranges.length; i++)
-              midB += msg.ranges[i];
-            log.info("A:" + midA + ", B:" + midB);
-            if (midA > midB) {
-              cmd.angular.z = -1;
-            } else {
-              cmd.angular.z = 1;
-            }
-          } else {
-            cmd.linear.x = 1;
-          }
-          publisher.publish(cmd);
-        }
-      }, LaserScan.class);
-      log.info("ready");
-    } catch (Exception e) {
-      if (node != null) {
-        node.getLog().fatal(e);
-      } else {
-        e.printStackTrace();
-      }
-    }
-  }
+	@Override
+	public void shutdown() {
+		node.shutdown();
+		node = null;
+	}
 
-  @Override
-  public void shutdown() {
-    node.shutdown();
-    node = null;
-  }
+	private class MyListener implements MessageListener<LaserScan> {
 
+		private final Log log;
+		private final Publisher<Twist> publisher;
+
+		public MyListener(Log log, Publisher<Twist> publisher) {
+			this.log = log;
+			this.publisher = publisher;
+		}
+
+		@Override
+		public void onNewMessage(LaserScan msg) {
+			Twist cmd = new Twist();
+			float[] ranges = msg.ranges;
+			if (obstacleIsClose(ranges)) {
+				cmd.angular.z = newAngle(ranges);
+			} else {
+				cmd.linear.x = 1;
+			}
+			publisher.publish(cmd);
+		}
+
+		private boolean obstacleIsClose(float[] ranges) {
+			for (int i = middle(ranges) - 15; i <= middle(ranges) + 15; i++) {
+				if (ranges[i] < 2.0) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private int middle(float[] ranges) {
+			return ranges.length / 2;
+		}
+
+		private int newAngle(float[] ranges) {
+			double midA = 0, midB = 0;
+			// we look to the left and to the right and decide which ones has
+			// more space
+			for (int i = 0; i < middle(ranges); i++) {
+				midA += ranges[i];
+			}
+			for (int i = middle(ranges); i < ranges.length; i++) {
+				midB += ranges[i];
+			}
+			log.info("A:" + midA + ", B:" + midB);
+			if (midA > midB) {
+				return -1;
+			} else {
+				return 1;
+			}
+		}
+	}
 }
-
